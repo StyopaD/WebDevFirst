20e5bb9092e400b68af9d59f91c1e2e7af17ba51
Stepan Batllori Martinez <stepan.batllori@students.salle.url.edu>
2021-08-05 17:51:15 +0200

    some queries

--- ThirdSession/js/basic.js
+++ ThirdSession/js/basic.js
@@ -1,10 +1,13 @@
-const mobileBtn = document-getElementById('mobile-cta')
-nav = document.querySelector('nav')
-mobileBtnExit = document.getElementById('mobile-exit');
+const mobileBtn = document.getElementById('mobile-cta')
+nav = document.querySelector('nav');
+mobileBtnExit = document.getElementById('mobile-exit')
+darkenWindow = document.getElementById('darken');
 
 mobileBtn.addEventListener('click', ()=>{
     nav.classList.add('menu-btn');
+    darkenWindow.style.display = "block";
 });
 mobileBtnExit.addEventListener('click', ()=>{
     nav.classList.remove('menu-btn');
+    darkenWindow.style.display = "none";
 });
